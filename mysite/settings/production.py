ea4a4a37978f74c392311e9d5940a7d5c072316b
hoshikuzu <hoshikuzu.debris@gmail.com>
2021-01-26 10:44:20 +0900

    my django app, first commit

---
+++ mysite/settings/production.py
@@ -0,0 +1,19 @@
+from .base import *
+import os
+import django_heroku, dj_database_url
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['.herokuapp.com']
+
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+django_heroku.settings(locals())
